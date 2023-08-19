b12041e1f2e8f41778b1d09e1ead0f12bcea62d0
Makserious44 <maksimmelnikov67@gmail.com>
2023-08-19 18:51:00 +0300

    v 0.1.7
    - added The Sofa
    - added bugs to fix later

--- VrRestorationProject/Assets/Scripts/ObjectReturn.cs
+++ VrRestorationProject/Assets/Scripts/ObjectReturn.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class ObjectReturn : MonoBehaviour
 {
     private Vector3 startPosition;
     private Quaternion startRotation;
+    private Interactable interactable;
     // Start is called before the first frame update
     void Start()
     {
